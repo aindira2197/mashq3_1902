2e6d7aa737723dc0cb3dc2432523c2c58ef3b0d2
Azadova Indira <aindira2197@gmail.com>
2026-02-19 14:35:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self._age = age
+
+    @property
+    def age(self):
+        return self._age
+
+    def set_age(self, value):
+        if value < 0:
+            return False
+        self._age = value
+        return True
